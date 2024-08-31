523572b22c7065cd943dff082f2a8c2bf90a1755
hanan-abdelmaseh <hananabdelmaseh9@gmail.com>
2024-08-31 21:08:11 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChooseUsComponent } from './components/choose-us/choose-us.component';
+import { AboutComponent } from './components/about/about.component';
+import { TeamComponent } from './components/team/team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChooseUsComponent,
+    AboutComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
